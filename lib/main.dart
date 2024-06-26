463292751bd5d3fc17aa36dffcef35f4804529c4
2004akhil26sharma <2004akhil26sharma@gmail.com>
2024-06-26 12:39:33 +0000

    Markers, Local Storage, Current Loc DONE

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,16 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:permission_handler/permission_handler.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: MapScreen(),
     );
@@ -16,82 +19,119 @@ class MyApp extends StatelessWidget {
 }
 
 class MapScreen extends StatefulWidget {
+  const MapScreen({super.key});
+
   @override
   _MapScreenState createState() => _MapScreenState();
 }
 
 class _MapScreenState extends State<MapScreen> {
-  Completer<GoogleMapController> _controller = Completer();
-  static const LatLng _center = const LatLng(45.521563, -122.677433);
-  final Set<Marker> _markers = {};
+  final Completer<GoogleMapController> _controller = Completer();
+  static const LatLng _center = LatLng(31.9779626, 76.7832618);
+  final Set<Marker> _markers = {}; // The marker array
   LatLng _lastMapPosition = _center;
 
+  Future<void> _requestPermissions() async { // requestion Location
+    await Permission.location.request();
+  }
+
+
   @override
-  void initState() {
+  void initState() { // things to start at initialization
     super.initState();
     _loadMarkers();
+    _requestPermissions(); 
   }
 
-  Future<void> _loadMarkers() async {
-    final prefs = await SharedPreferences.getInstance();
-    final markersData = prefs.getStringList('markers') ?? [];
-    setState(() {
-      _markers.clear();
-      for (String markerData in markersData) {
-        final data = markerData.split(',');
-        final marker = Marker(
-          markerId: MarkerId(data[0]),
-          position: LatLng(double.parse(data[1]), double.parse(data[2])),
-          infoWindow: InfoWindow(title: data[3]),
-          icon: BitmapDescriptor.defaultMarker,
-        );
-        _markers.add(marker);
-      }
-    });
-  }
-
-  Future<void> _addMarkerWithDetails(LatLng position) async {
-    final TextEditingController controller = TextEditingController();
-    final prefs = await SharedPreferences.getInstance();
-    showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: Text('Enter Details'),
-          content: TextField(
-            controller: controller,
-            decoration: InputDecoration(hintText: "Details"),
-          ),
-          actions: <Widget>[
-            TextButton(
-              child: Text('Cancel'),
-              onPressed: () => Navigator.of(context).pop(),
+Future<void> _addMarkerWithDetails(LatLng position) async {
+  final TextEditingController controller = TextEditingController();
+  final TextEditingController controller2 = TextEditingController();
+  final prefs = await SharedPreferences.getInstance();
+  showDialog(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        title: const Text('Enter Details for this location'),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextField(
+              controller: controller,
+              decoration: const InputDecoration(hintText: "Title"),
             ),
-            TextButton(
-              child: Text('Save'),
-              onPressed: () async {
-                final String details = controller.text;
-                setState(() {
-                  _markers.add(Marker(
-                    markerId: MarkerId(position.toString()),
-                    position: position,
-                    infoWindow: InfoWindow(title: details),
-                    icon: BitmapDescriptor.defaultMarker,
-                  ));
-                });
-                // Save to local storage
-                final String markerData = '${position.toString()},${position.latitude},${position.longitude},$details';
-                final List<String> markersData = prefs.getStringList('markers') ?? [];
-                markersData.add(markerData);
-                await prefs.setStringList('markers', markersData);
-                Navigator.of(context).pop();
-              },
+            const SizedBox(height: 10),
+            TextField(
+              controller: controller2,
+              decoration: const InputDecoration(hintText: "Details"),
             ),
           ],
-        );
-      },
-    );
-  }
+        ),
+        actions: <Widget>[
+          TextButton(
+            child: const Text('Cancel'),
+            onPressed: () => Navigator.of(context).pop(),
+          ),
+          TextButton(
+            child: const Text('Save'),
+            onPressed: () async {
+              final String title = controller.text;
+              final String details = controller2.text;
+              setState(() {
+                _markers.add(Marker(
+                  markerId: MarkerId(position.toString()),
+                  position: position,
+                  infoWindow: InfoWindow(title: title, snippet: details),
+                  icon: BitmapDescriptor.defaultMarker,
+                ));
+              });
+              // Save to local storage
+              final String markerData = '${position.latitude},${position.longitude},$title,$details';
+              final List<String> markersData = prefs.getStringList('markers') ?? [];
+              print("Saving marker data: $markerData");
+              markersData.add(markerData);
+              await prefs.setStringList('markers', markersData);
+              final List<String>? debugMarkersData = prefs.getStringList('markers');
+              print("Markers data after saving: $debugMarkersData");
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
+      );
+    },
+  );
+}
+
+Future<void> _loadMarkers() async {
+  final prefs = await SharedPreferences.getInstance();
+  final markersData = prefs.getStringList('markers') ?? [];
+  setState(() {
+    _markers.clear(); // for clearing the markers because we are adding new ones
+
+
+    // I was getting the error in loading markers because the data is not in the correct format. The data was in LatLng and I was directly converting into doubles.
+    for (String it in markersData) {
+      final data = it.split(',');
+      if (data.length >= 4) { 
+        try {
+          final double lat = double.parse(data[0]);
+          final double lng = double.parse(data[1]);
+          final String title = data[2];
+          final String details = data[3];
+          final marker = Marker(
+            markerId: MarkerId('$lat,$lng'),
+            position: LatLng(lat, lng),
+            infoWindow: InfoWindow(title: title, snippet: details),
+            icon: BitmapDescriptor.defaultMarker,
+          );
+          _markers.add(marker);
+        } catch (e) {
+          // Handle or log error
+          print("Error parsing marker data: $e");
+        }
+      }
+    }
+  });
+}
 
   void _onAddMarkerButtonPressed() async {
     _addMarkerWithDetails(_lastMapPosition);
@@ -105,11 +145,11 @@ class _MapScreenState extends State<MapScreen> {
     _controller.complete(controller);
   }
 
-   @override
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Geocoding'),
+        title: const Text('Atomic Maps'),
       ),
       body: Stack(
         children: <Widget>[
@@ -117,10 +157,15 @@ class _MapScreenState extends State<MapScreen> {
             onMapCreated: _onMapCreated,
             initialCameraPosition: const CameraPosition(
               target: _center,
-              zoom: 11.0,
+              zoom: 15.0,
             ),
             markers: _markers,
             onCameraMove: _onCameraMove,
+            compassEnabled: true,
+            mapType: MapType.hybrid,
+            myLocationEnabled: true,
+            myLocationButtonEnabled: true,
+            onTap: (argument) => _addMarkerWithDetails(argument),
           ),
           Padding(
             padding: const EdgeInsets.all(16.0),
